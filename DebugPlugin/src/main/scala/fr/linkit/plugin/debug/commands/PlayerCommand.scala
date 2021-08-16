e4d5f8002d4bb435bb3b4d1b8ad6116c49fc40ef
Override-6 <maximebatista18@gmail.com>
2021-08-16 23:20:09 +0200

    Added javadoc and fixed compilation errors

--- DebugPlugin/src/main/scala/fr/linkit/plugin/debug/commands/PlayerCommand.scala
+++ DebugPlugin/src/main/scala/fr/linkit/plugin/debug/commands/PlayerCommand.scala
@@ -33,7 +33,7 @@ class PlayerCommand(cacheHandler: SharedCacheManager, currentIdentifier: String)
     }.build
 
     private val repo    = cacheHandler.attachToCache(50, DefaultSynchronizedObjectCenter[ListBuffer[Player]](tree))
-    private val players = repo.getOrPost(0, ListBuffer.empty[Player])
+    private val players = repo.getOrPost(0)(ListBuffer.empty[Player])
     println(s"players = ${players}")
     /*println(s"players.getClass.getDeclaredFields = ${players.getClass.getDeclaredFields.mkString("Array(", ", ", ")")}")
     println(s"LOL")*/
